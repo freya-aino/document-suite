bf746aa20ff6596df2f2fac657616b0b8a7e41f9
freya <freya.aino@pm.me>
2026-03-07 19:57:03 +0100

    first migration sqlc generate (not tested)

--- workflow/migration/migrations/000001_document_model.up.sql
+++ workflow/migration/migrations/000001_document_model.up.sql
@@ -1,61 +1,66 @@
 -- HUBS
 
-CREATE TABLE IF NOT EXISTS HubDocument (
-    UUID UUID,
-    LDTS timestamptz,
-    UDTS timestamptz
+CREATE TABLE IF NOT EXISTS Hub_Document (
+    UUID UUID PRIMARY KEY NOT NULL,
+    LDTS timestamptz NOT NULL,
+    UDTS timestamptz NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS HubDocumentPage (
-    UUID UUID,
-    LDTS timestamptz,
-    UDTS timestamptz,
-    DocumentPageIndex int2
+CREATE TABLE IF NOT EXISTS Hub_Document_Page (
+    UUID UUID PRIMARY KEY NOT NULL,
+    LDTS timestamptz NOT NULL,
+    UDTS timestamptz NOT NULL,
+    DocumentPageIndex int2 NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS HubTextChunk (
-	UUID UUID,
-	LDTS timestamptz,
-    UDTS timestamptz,
-	Content text
+CREATE TABLE IF NOT EXISTS Hub_Text_Chunk (
+	UUID UUID PRIMARY KEY NOT NULL,
+	LDTS timestamptz NOT NULL,
+    UDTS timestamptz NOT NULL,
+	Content text NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS HubVectorization (
-    UUID UUID,
-    LDTS timestamptz,
-    UDTS timestamptz
+CREATE TABLE IF NOT EXISTS Hub_Vectorization (
+    UUID UUID PRIMARY KEY NOT NULL,
+    LDTS timestamptz NOT NULL,
+    UDTS timestamptz NOT NULL
 );
 
 -- SATELITES
 
-CREATE TABLE IF NOT EXISTS SatDocumentMetadata (
-    DocumentUUID UUID,
-    LDTS timestamptz,
-    UDTS timestamptz,
+CREATE TABLE IF NOT EXISTS Sat_Document_Metadata (
+    DocumentUUID UUID PRIMARY KEY NOT NULL,
+    LDTS timestamptz NOT NULL,
+    UDTS timestamptz NOT NULL,
 
-    DocumentHash varchar(128),
-    Name varchar(250)
+    DocumentHash varchar(128) NOT NULL,
+    Name varchar(250) NOT NULL
 );
-
 
 -- LINKS
 
-CREATE TABLE IF NOT EXISTS LnkDocumentDocumentPage (
-    DocumentUUID UUID,
-    DocumentPageUUID UUID,
-    LDTS timestamptz
+CREATE TABLE IF NOT EXISTS Lnk_Document_Document_Page (
+    DocumentUUID UUID NOT NULL,
+    DocumentPageUUID UUID NOT NULL,
+    LDTS timestamptz NOT NULL,
+
+    PRIMARY KEY (DocumentUUID, DocumentPageUUID)
 );
 
-CREATE TABLE IF NOT EXISTS LnkDocumentTextChunk (
-    DocumentUUID UUID,
-    TextChunkUUID UUID,
-    LDTS timestamptz
+CREATE TABLE IF NOT EXISTS Lnk_Document_Text_Chunk (
+    DocumentUUID UUID NOT NULL,
+    TextChunkUUID UUID NOT NULL,
+    LDTS timestamptz NOT NULL,
+
+    PRIMARY KEY (DocumentUUID, TextChunkUUID)
 );
 
-CREATE TABLE IF NOT EXISTS LnkTextChunkVectorization (
-    TextChunkUUID UUID,
-    VectorizationUUID UUID,
-    LDTS timestamptz
+CREATE TABLE IF NOT EXISTS Lnk_Text_Chunk_Vectorization (
+    TextChunkUUID UUID NOT NULL,
+    VectorizationUUID UUID NOT NULL,
+    LDTS timestamptz NOT NULL,
+
+    PRIMARY KEY (TextChunkUUID, VectorizationUUID)
 );
 
 -- CREATE TABLE IF NOT EXISTS HubVectorQueryResult
